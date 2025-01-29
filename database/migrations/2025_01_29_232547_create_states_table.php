925af8ba26ca064fcb0e6fcdf6b9679d17daca48
Adrian Canals <a.canals14@gmail.com>
2025-01-29 23:27:12 +0100

    Publicar migraciones

---
+++ database/migrations/2025_01_29_232547_create_states_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('states')) {
+            Schema::create('states', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->boolean('finalized');
+                $table->string('code')->unique();
+                $table->string('type');
+                $table->string('icon')->nullable();
+                $table->timestamps();
+                $table->unique(["name", "type"], 'name_type_unique');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('states');
+    }
+}
