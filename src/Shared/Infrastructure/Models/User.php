a4e6f86d4550ba2b9ea041cbdae6aa5443d40763
Adrian Canals <a.canals14@gmail.com>
2025-02-06 12:54:26 +0100

    hexagonalDevelop: (example) Nuevos archivos de Src para los ejemplos

--- src/Shared/Infrastructure/Models/User.php
+++ src/Shared/Infrastructure/Models/User.php
@@ -1,16 +1,22 @@
 <?php
 
-namespace App\Models;
+declare(strict_types=1);
+
+namespace Src\Shared\Infrastructure\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    static string $factory = UserFactory::class;
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +50,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
