1e3075f66200b0118a20276cdae01527a489a975
Adrian Canals <a.canals14@gmail.com>
2025-02-13 13:40:32 +0100

    tsUtilsDevelop: Añadir variable de configuración "VITE_TS_USE_BOOSTRAP_CLASSES" para obtener la "hiddenClass" dinámicamente

--- resources/js/app/constants.ts
+++ resources/js/app/constants.ts
@@ -6,17 +6,17 @@ interface AppConstants extends DefaultConstants {
 
 const constants = Constants.getInstance<AppConstants>();
 constants.extend({
-    appIcon:                    new URL('/resources/images/favicon.ico', import.meta.url).href,
-    VITE_BROADCASTING_ENABLED:  import.meta.env.VITE_BROADCASTING_ENABLED === 'true',
-    VITE_REVERB_APP_KEY:        import.meta.env.VITE_REVERB_APP_KEY,
-    VITE_REVERB_HOST:           import.meta.env.VITE_REVERB_HOST,
-    VITE_REVERB_PORT:           parseInt(import.meta.env.VITE_REVERB_PORT),
-    VITE_REVERB_SCHEME:         import.meta.env.VITE_REVERB_SCHEME,
-    VITE_APP_ENV:               import.meta.env.VITE_APP_ENV,
-    VITE_APP_NAME:              import.meta.env.VITE_APP_NAME,
-    VITE_TS_STORAGE_VERSION:    import.meta.env.VITE_TS_STORAGE_VERSION,
-
-    anotherSetting:             'newCustomValue',
+    appIcon:                      new URL('/resources/images/favicon.ico', import.meta.url).href,
+    VITE_BROADCASTING_ENABLED:    import.meta.env.VITE_BROADCASTING_ENABLED === 'true',
+    VITE_REVERB_APP_KEY:          import.meta.env.VITE_REVERB_APP_KEY,
+    VITE_REVERB_HOST:             import.meta.env.VITE_REVERB_HOST,
+    VITE_REVERB_PORT:             parseInt(import.meta.env.VITE_REVERB_PORT),
+    VITE_REVERB_SCHEME:           import.meta.env.VITE_REVERB_SCHEME,
+    VITE_APP_ENV:                 import.meta.env.VITE_APP_ENV,
+    VITE_APP_NAME:                import.meta.env.VITE_APP_NAME,
+    VITE_TS_STORAGE_VERSION:      import.meta.env.VITE_TS_STORAGE_VERSION,
+    VITE_TS_USE_BOOSTRAP_CLASSES: import.meta.env.VITE_TS_USE_BOOSTRAP_CLASSES === 'true',
+    anotherSetting:               'newCustomValue',
 });
 
 export const _const = <T extends keyof AppConstants>(key: T): AppConstants[T] => constants.get(key);
