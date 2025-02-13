68ea44904d236b63cc42ed6aa70ae821d8e929f2
Adrián <acanals@externos.itnow.es>
2025-02-13 09:24:14 +0100

    tsUtilsDevelop: (example) Variables VITE ordenadas

--- resources/js/app/constants.ts
+++ resources/js/app/constants.ts
@@ -7,13 +7,11 @@ interface AppConstants extends DefaultConstants {
 const constants = Constants.getInstance<AppConstants>();
 constants.extend({
     appIcon:                    new URL('/resources/images/favicon.ico', import.meta.url).href,
-
+    VITE_BROADCASTING_ENABLED:  import.meta.env.VITE_BROADCASTING_ENABLED === 'true',
     VITE_REVERB_APP_KEY:        import.meta.env.VITE_REVERB_APP_KEY,
     VITE_REVERB_HOST:           import.meta.env.VITE_REVERB_HOST,
     VITE_REVERB_PORT:           parseInt(import.meta.env.VITE_REVERB_PORT),
     VITE_REVERB_SCHEME:         import.meta.env.VITE_REVERB_SCHEME,
-
-    VITE_BROADCASTING_ENABLED:  import.meta.env.VITE_BROADCASTING_ENABLED === 'true',
     VITE_APP_ENV:               import.meta.env.VITE_APP_ENV,
     VITE_APP_NAME:              import.meta.env.VITE_APP_NAME,
     VITE_APP_STORAGE_VERSION:   import.meta.env.VITE_APP_STORAGE_VERSION,
