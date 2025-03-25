31c870bd60760fb1f6395bf7507ac7707d06ee92
Adrian Canals <a.canals14@gmail.com>
2025-03-25 11:47:57 +0100

    kalionDevelop: eliminar la configuración de las cookies del AppServiceProvider, ya que ahora se usan las configuraciones

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,8 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        \Thehouseofel\Kalion\Infrastructure\Services\Kalion::configure()
-            ->enablePreferencesCookie();
+        //
     }
 
     /**
