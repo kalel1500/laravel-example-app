dbfaa6ee50e16f204f121eab08cad54249702e37
Adrian Canals <a.canals14@gmail.com>
2025-04-07 13:04:49 +0200

    kalionDevelop: eliminar "UserRepositoryContract"

--- src/Shared/Infrastructure/Repositories/Eloquent/UserRepository.php
+++ src/Shared/Infrastructure/Repositories/Eloquent/UserRepository.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace Src\Shared\Infrastructure\Repositories\Eloquent;
 
-use Src\Shared\Domain\Contracts\Repositories\UserRepositoryContract;
 use Src\Shared\Domain\Objects\Entities\UserEntity;
 use \Thehouseofel\Kalion\Infrastructure\Repositories\UserRepository as BaseUserRepository;
 
-final class UserRepository extends BaseUserRepository implements UserRepositoryContract
+final class UserRepository extends BaseUserRepository
 {
     public function is_important_group(UserEntity $user): bool
     {
