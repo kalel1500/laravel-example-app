21760dbc61156cbf29a659d88556d25ad3790e37
Adrian Canals <a.canals14@gmail.com>
2025-04-16 09:45:22 +0200

    kalionDevelop: Extender modelo "User" del paquete (y borrar el código repetido)

--- src/Shared/Infrastructure/Models/User.php
+++ src/Shared/Infrastructure/Models/User.php
@@ -4,55 +4,11 @@ declare(strict_types=1);
 
 namespace Src\Shared\Infrastructure\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Thehouseofel\Kalion\Domain\Traits\ModelHasPermissions;
+use Thehouseofel\Kalion\Infrastructure\Models\User as KalionUser;
 
-class User extends Authenticatable
+class User extends KalionUser
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, ModelHasPermissions;
-
-    static string $factory = UserFactory::class;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
-
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
