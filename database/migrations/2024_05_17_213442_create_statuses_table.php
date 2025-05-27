724c965767c34e6e2581c091f53863878e85bf67
Adrian Canals <a.canals14@gmail.com>
2025-05-27 13:00:05 +0200

    kalionDevelop: tabla "states" renombrada a "statuses"

--- database/migrations/2024_05_17_213442_create_statuses_table.php
+++ database/migrations/2024_05_17_213442_create_statuses_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('states', function (Blueprint $table) {
+        Schema::create('statuses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->boolean('finalized');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('states');
+        Schema::dropIfExists('statuses');
     }
 };
