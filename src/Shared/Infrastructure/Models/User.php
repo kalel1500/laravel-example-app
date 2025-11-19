af15305bfcc40fc7a83647b895cb590811a36d61
Adrian Canals <a.canals14@gmail.com>
2025-11-19 19:19:41 +0100

    install api

--- src/Shared/Infrastructure/Models/User.php
+++ src/Shared/Infrastructure/Models/User.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Src\Shared\Infrastructure\Models;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laravel\Sanctum\HasApiTokens;
 use Thehouseofel\Kalion\Features\Shared\Infrastructure\Models\User as KalionUser;
 
 class User extends KalionUser
 {
+    use HasApiTokens;
+
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
